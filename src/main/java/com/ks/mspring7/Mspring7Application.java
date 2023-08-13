92be20b3f6632d3fe05ea965c4c2d89d01055ec4
Mahendra Bhandarkar <mahendra.bhandarkar@expleogroup.com>
2023-08-13 21:23:04 +0530

    first commit

---
+++ src/main/java/com/ks/mspring7/Mspring7Application.java
@@ -0,0 +1,13 @@
+package com.ks.mspring7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mspring7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mspring7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ks/mspring7/Mspring7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.mspring7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mspring7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
